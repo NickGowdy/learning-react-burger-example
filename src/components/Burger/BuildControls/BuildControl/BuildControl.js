ea74b593172f5ed7822e7119495b15b4a021ef25
Nick Gowdy <nickgowdy87@gmail.com>
2019-01-12 15:49:35 +0000

    Adding basic builder controls. CSS is a bit FUBAR right now.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,8 +3,8 @@ import React from 'react'
 import classes from './BuildControl.css'
 
 const buildControl = (props) => {
-  return <div className={classes.buildControl}>
-    <div className={classes.label}>
+  return <div className={classes.BuildControl}>
+    <div className={classes.Label}>
       {props.label}
     </div>
     <button className={classes.Less}>Less</button>
